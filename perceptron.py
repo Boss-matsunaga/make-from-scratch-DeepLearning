1b38eff802890b146818b9e6bc01c331ff125b03
shunsuke_ac6328mats <shunmametarou@gmail.com>
2020-04-26 13:11:15 +0900

    XOR関数作成

--- perceptron.py
+++ perceptron.py
@@ -31,7 +31,13 @@ def OR(x1, x2):
     else:
         return 1
 
-print(AND(0, 0))
-print(AND(1, 0))
-print(AND(0, 1))
-print(AND(1, 1))
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+print(XOR(0, 0))
+print(XOR(1, 0))
+print(XOR(0, 1))
+print(XOR(1, 1))
