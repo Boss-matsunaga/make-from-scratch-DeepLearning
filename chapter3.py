21281a3152e1026835cdb21048e4cbb46862eb96
shunsuke_ac6328mats <shunmametarou@gmail.com>
2020-04-26 13:48:06 +0900

    複数グラフ表示するようにした

--- chapter3.py
+++ chapter3.py
@@ -9,8 +9,21 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 x = np.arange(-5.0, 5.0, 0.1)
-# y = step_function(x)
-y = sigmoid(x)
-plt.plot(x, y)
-plt.ylim(-0.1, 1.1)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 2, 1)
+ax2 = fig.add_subplot(1, 2, 2)
+
+y1 = step_function(x)
+y2 = sigmoid(x)
+
+ax1.plot(x, y1)
+ax2.plot(x, y2)
+
+ax1.set_ylim(-0.1, 1.1)
+ax2.set_ylim(-0.1, 1.1)
+
+ax1.set_title("step")
+ax2.set_title("sigmoid")
+
 plt.show()
