994edab9a47a7e494b5208d5ce0d8fe841950513
shunsuke_ac6328mats <shunmametarou@gmail.com>
2020-04-27 11:40:55 +0900

    softmaxのオーバーフロー対策

--- chapter3/chapter3_output_layer.py
+++ chapter3/chapter3_output_layer.py
@@ -2,8 +2,11 @@ import numpy as np
 
 
 def softmax(a):
-    exp_a = np.exp(a)
-    sum_exp_a = np.sum(a)
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
 
     return y
+
+print(softmax(np.array([0.3, 2.9, 4.0])))
